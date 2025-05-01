08baec5347d72ed170221d30c6dbf42215190b77
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-05-01 15:49:26 +0100

    added json import

--- test_dino.py
+++ test_dino.py
@@ -5,6 +5,7 @@ import tqdm
 import os
 import matplotlib.pyplot as plt
 from datetime import datetime
+import json
 
 model_path = r'/root/field_data/test/ml/cg/DINO Models'
 train_path = r'/root/docker_data/Autoencoder/hdf5/train.hdf5'
