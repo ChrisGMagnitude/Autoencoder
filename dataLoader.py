0d76cb3b0f835e5736b944262afa6d27d0fe2516
ChrisGMagnitude <c.griffiths@magnitudesurveys.co.uk>
2025-05-01 11:02:58 +0100

    Removed more references to labels

--- dataLoader.py
+++ dataLoader.py
@@ -14,7 +14,7 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 class MagClassDataset(Dataset):
 
     def __init__(self, hdf5_file, 
-                 augment=True, crop_ranges=[[-1,2],[-3,5],[-10,20]], crop_jitter=[0.25,0.5,2], max_white_noise=0.05,binary_label=True):
+                 augment=True, crop_ranges=[[-1,2],[-3,5],[-10,20]], crop_jitter=[0.25,0.5,2], max_white_noise=0.05):
         """
         Arguments:
             csv_file (string): Path to the HDF5 file.
@@ -23,7 +23,6 @@ class MagClassDataset(Dataset):
         """
         self.hdf5_file = hdf5_file
         self.fh = h5py.File(self.hdf5_file, "r")
-        self.classes = np.unique(self.labels)
         self.crop_ranges = crop_ranges
         self.crop_jitter = crop_jitter
         self.max_white_noise = max_white_noise
@@ -34,7 +33,7 @@ class MagClassDataset(Dataset):
         
         
     def __len__(self):
-        return len(self.labels)
+        return len(self.fh["images"])
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
@@ -107,34 +106,4 @@ class MagClassDataset(Dataset):
         image = transformer(image)
         
         return image.type(torch.float)
-        
-        
-def make_weights_for_balanced_classes(classes, nclasses):  
-    classes = classes.astype(int)
-    count = [0] * nclasses       
-    for item in classes:          
-        count[item] += 1                                                     
-    weight_per_class = [0.] * nclasses                                      
-    N = float(sum(count))                                                   
-    for i in range(nclasses):                                                   
-        weight_per_class[i] = N/float(count[i])                                 
-    weight = [0] * len(classes)                                              
-    for idx, val in enumerate(classes):                     
-        weight[idx] = weight_per_class[val]                                  
-    return weight
-
-
-
-                                                        
-def get_weighted_data_loader(dataset,epoch_size,batch_size,num_workers=40):                                                        
-    # For unbalanced dataset we create a weighted sampler   
-    weights = make_weights_for_balanced_classes(dataset.labels, len(dataset.classes))     
-                                                            
-    weights = torch.DoubleTensor(weights)
-    
-    sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, epoch_size)#len(weights))                     
-                                                                                    
-    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,                               
-                                             sampler = sampler, pin_memory=True,num_workers=num_workers)      
-    return data_loader
-                                             
+           
